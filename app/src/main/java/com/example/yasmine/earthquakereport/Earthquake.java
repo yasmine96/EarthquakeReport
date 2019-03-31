270552e60b44748043c033310269ed5af509ba19
yasmine96 <yasminemohamedelsayed@gmail.com>
2019-03-31 18:26:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/yasmine/earthquakereport/Earthquake.java
@@ -0,0 +1,38 @@
+package com.example.yasmine.earthquakereport;
+
+public class Earthquake{
+
+    private double mMagnitude;
+    private String mLocation;
+    private long mTimeInMilliseconds;
+    private String mUrl;
+
+    public Earthquake(double magnitude , String location ,  long TimeInMilliseconds ,String url ){
+
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = TimeInMilliseconds;
+        mUrl = url;
+    }
+
+
+    public double getMagnitude(){
+
+        return mMagnitude;
+    }
+
+    public String getLocation(){
+
+        return mLocation;
+    }
+
+    public long getTimeInMilliseconds(){
+
+        return mTimeInMilliseconds;
+    }
+
+    public String getUrl(){
+
+        return mUrl;
+    }
+}
